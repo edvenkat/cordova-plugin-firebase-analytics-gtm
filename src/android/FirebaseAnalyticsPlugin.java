9f21cf5b6405396b90d77f7cc9301236a53a6896
Venka Tesh G <gvenkatesh225@gmail.com>
2019-08-19 11:48:13 +0530

    Update FirebaseAnalyticsPlugin.java

--- src/android/FirebaseAnalyticsPlugin.java
+++ src/android/FirebaseAnalyticsPlugin.java
@@ -69,7 +69,7 @@ public class FirebaseAnalyticsPlugin extends CordovaPlugin {
             bundle.putString(FirebaseAnalytics.Param.ITEM_ID, params.get("item_id").toString());
             bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, params.get("item_name").toString());
             bundle.putString(FirebaseAnalytics.Param.ITEM_CATEGORY, params.get("item_category").toString());
-            bundle.putFloat(FirebaseAnalytics.Param.QUANTITY, ((Number) params.get("quantity")).floatValue());
+            //bundle.putFloat(FirebaseAnalytics.Param.QUANTITY, ((Number) params.get("quantity")).floatValue());
             bundle.putString(FirebaseAnalytics.Param.ITEM_LOCATION_ID, params.get("item_location_id").toString());
             this.firebaseAnalytics.logEvent(FirebaseAnalytics.Event.ADD_TO_WISHLIST, bundle);
         } else if(name.equalsIgnoreCase("addtocart")) {
